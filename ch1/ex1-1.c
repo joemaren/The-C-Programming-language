c3d140611bcd64bf6205c51822372de81cdca015
Iya <13188600+joemaren@users.noreply.github.com>
2024-03-16 14:48:08 +0100

    Exercise 1-1

---
+++ ch1/ex1-1.c
@@ -0,0 +1,15 @@
+/* ex1-c
+ * Exercise 1-1. Run the "hello, world" program on your system. 
+ * Experiment with leaving out parts of the program, 
+ * to see what error message you get.
+ */
+
+/* Written by Joseph Kurape. 
+ */
+
+/* #include <stdio.h>      // comiler can't find function printf() fucntion delaration */
+
+main()                              // return type warning by compiler 
+{
+    printf("hello, world\n");
+}
