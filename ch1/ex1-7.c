9e89ac25d1fb04046bfd4cfa22a4bd3c6505fd7c
Iya <13188600+joemaren@users.noreply.github.com>
2024-03-17 01:06:08 +0100

    Exercise 1-7

---
+++ ch1/ex1-7.c
@@ -0,0 +1,14 @@
+/* ex1-7
+ * Exercise 1-7. Write a program to print the value of EOF.
+ */
+
+/*Written by Joseph Kurape
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Value of \"EOF\" is %d\n", EOF);    /* prints the numerical value of EOF */
+    return 0;
+}
